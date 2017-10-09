19b681cf392e8d4f90381ca6738035388106887b
Dag Robole <dag.robole@gmail.com>
2017-10-10 01:37:42 +0200

    retab

--- rpt2chn.go
+++ rpt2chn.go
@@ -19,156 +19,156 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/binary"
-	"errors"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "bytes"
+    "encoding/binary"
+    "errors"
+    "flag"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
 )
 
 var (
-	inFile  string
-	outFile string
+    inFile  string
+    outFile string
 )
 
 func init() {
 
-	flag.StringVar(&inFile, "if", "", "RPT file to read from")
-	flag.StringVar(&outFile, "of", "", "CHN file to write to")
+    flag.StringVar(&inFile, "if", "", "RPT file to read from")
+    flag.StringVar(&outFile, "of", "", "CHN file to write to")
 }
 
 func main() {
 
-	flag.Parse()
+    flag.Parse()
 
-	if flag.NFlag() != 2 || len(inFile) == 0 || len(outFile) == 0 {
-		flag.Usage()
-		os.Exit(1)
-	}
+    if flag.NFlag() != 2 || len(inFile) == 0 || len(outFile) == 0 {
+        flag.Usage()
+        os.Exit(1)
+    }
 
-	dieIf := func(err error) {
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(1)
-		}
-	}
+    dieIf := func(err error) {
+        if err != nil {
+            fmt.Fprintln(os.Stderr, err.Error())
+            os.Exit(1)
+        }
+    }
 
-	_, err := os.Stat(inFile)
-	dieIf(err)
+    _, err := os.Stat(inFile)
+    dieIf(err)
 
-	fin, err := os.Open(inFile)
-	dieIf(err)
-	defer fin.Close()
+    fin, err := os.Open(inFile)
+    dieIf(err)
+    defer fin.Close()
 
-	scanner := bufio.NewScanner(fin)
+    scanner := bufio.NewScanner(fin)
 
-	scanner.Scan()
-	dieIf(scanner.Err())
-	dateTime, seconds, err := parseAquisitionDate(scanner.Text())
-	dieIf(err)
+    scanner.Scan()
+    dieIf(scanner.Err())
+    dateTime, seconds, err := parseAquisitionDate(scanner.Text())
+    dieIf(err)
 
-	scanner.Scan()
-	dieIf(scanner.Err())
-	livetime, err := parseTrailingFloat(scanner.Text())
-	dieIf(err)
+    scanner.Scan()
+    dieIf(scanner.Err())
+    livetime, err := parseTrailingFloat(scanner.Text())
+    dieIf(err)
 
-	scanner.Scan()
-	dieIf(scanner.Err())
-	realtime, err := parseTrailingFloat(scanner.Text())
-	dieIf(err)
+    scanner.Scan()
+    dieIf(scanner.Err())
+    realtime, err := parseTrailingFloat(scanner.Text())
+    dieIf(err)
 
-	channelBuffer := new(bytes.Buffer)
-	for scanner.Scan() {
-		err := absorbChannels(scanner.Text(), channelBuffer)
-		dieIf(err)
-	}
-	dieIf(scanner.Err())
+    channelBuffer := new(bytes.Buffer)
+    for scanner.Scan() {
+        err := absorbChannels(scanner.Text(), channelBuffer)
+        dieIf(err)
+    }
+    dieIf(scanner.Err())
 
-	numChannels := channelBuffer.Len() / 4
-	if numChannels == 0 || numChannels&(numChannels-1) != 0 {
-		dieIf(errors.New("number of channels is not a power of two"))
-	}
+    numChannels := channelBuffer.Len() / 4
+    if numChannels == 0 || numChannels&(numChannels-1) != 0 {
+        dieIf(errors.New("number of channels is not a power of two"))
+    }
 
-	fout, err := os.Create(outFile)
-	dieIf(err)
-	defer fout.Close()
+    fout, err := os.Create(outFile)
+    dieIf(err)
+    defer fout.Close()
 
-	// Header
-	binary.Write(fout, binary.LittleEndian, int16(-1))
-	binary.Write(fout, binary.LittleEndian, int16(1))
-	binary.Write(fout, binary.LittleEndian, int16(1))
-	fout.Write(seconds)
-	binary.Write(fout, binary.LittleEndian, int32(realtime*50.0))
-	binary.Write(fout, binary.LittleEndian, int32(livetime*50.0))
-	fout.Write(dateTime)
-	binary.Write(fout, binary.LittleEndian, int16(0))
-	binary.Write(fout, binary.LittleEndian, int16(numChannels))
+    // Header
+    binary.Write(fout, binary.LittleEndian, int16(-1))
+    binary.Write(fout, binary.LittleEndian, int16(1))
+    binary.Write(fout, binary.LittleEndian, int16(1))
+    fout.Write(seconds)
+    binary.Write(fout, binary.LittleEndian, int32(realtime*50.0))
+    binary.Write(fout, binary.LittleEndian, int32(livetime*50.0))
+    fout.Write(dateTime)
+    binary.Write(fout, binary.LittleEndian, int16(0))
+    binary.Write(fout, binary.LittleEndian, int16(numChannels))
 
-	// Spectrum
-	fout.Write(channelBuffer.Bytes())
+    // Spectrum
+    fout.Write(channelBuffer.Bytes())
 
-	// Footer
-	binary.Write(fout, binary.LittleEndian, int16(-102))
+    // Footer
+    binary.Write(fout, binary.LittleEndian, int16(-102))
     fout.Write(make([]byte, 510))
 }
 
 func parseAquisitionDate(line string) ([]byte, []byte, error) {
 
-	items := strings.Split(strings.Trim(line, " \t\n"), " ")
-	if len(items) < 4 {
-		return nil, nil, errors.New("ParseAquisitionDate: missing items")
-	}
+    items := strings.Split(strings.Trim(line, " \t\n"), " ")
+    if len(items) < 4 {
+        return nil, nil, errors.New("ParseAquisitionDate: missing items")
+    }
 
-	dt, tm := items[2], items[3]
-	if len(dt) != 10 || len(tm) != 8 {
-		return nil, nil, errors.New("ParseAquisitionDate: date/time format invalid")
-	}
+    dt, tm := items[2], items[3]
+    if len(dt) != 10 || len(tm) != 8 {
+        return nil, nil, errors.New("ParseAquisitionDate: date/time format invalid")
+    }
 
-	month, err := strconv.Atoi(dt[3:5])
-	if err != nil {
-		return nil, nil, err
-	}
+    month, err := strconv.Atoi(dt[3:5])
+    if err != nil {
+        return nil, nil, err
+    }
 
-	if month < 1 || month > 12 {
-		return nil, nil, errors.New("ParseAquisitionDate: month out of range")
-	}
+    if month < 1 || month > 12 {
+        return nil, nil, errors.New("ParseAquisitionDate: month out of range")
+    }
 
-	monthNames := [...]string{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}
+    monthNames := [...]string{"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"}
 
-	dateTimeParts := [][]byte{[]byte(dt[:2]), []byte(monthNames[month-1]), []byte(dt[8:10]), []byte("1"), []byte(tm[:2]), []byte(tm[3:5])}
+    dateTimeParts := [][]byte{[]byte(dt[:2]), []byte(monthNames[month-1]), []byte(dt[8:10]), []byte("1"), []byte(tm[:2]), []byte(tm[3:5])}
 
-	return bytes.Join(dateTimeParts, []byte("")), []byte(tm[6:8]), nil
+    return bytes.Join(dateTimeParts, []byte("")), []byte(tm[6:8]), nil
 }
 
 func parseTrailingFloat(line string) (float64, error) {
 
-	items := strings.Split(strings.Trim(line, " \t\n"), " ")
-	if len(items) == 0 {
-		return 0.0, errors.New("ParseTrailingFloat: no valid decimal found")
-	}
+    items := strings.Split(strings.Trim(line, " \t\n"), " ")
+    if len(items) == 0 {
+        return 0.0, errors.New("ParseTrailingFloat: no valid decimal found")
+    }
 
-	return strconv.ParseFloat(items[len(items)-1], 32)
+    return strconv.ParseFloat(items[len(items)-1], 32)
 }
 
 func absorbChannels(line string, w io.Writer) error {
 
-	items := strings.Fields(strings.Trim(line, " \t\n"))
-	if len(items) < 2 {
-		return nil
-	}
+    items := strings.Fields(strings.Trim(line, " \t\n"))
+    if len(items) < 2 {
+        return nil
+    }
 
-	for _, v := range items[1:] {
-		ch, err := strconv.Atoi(v)
-		if err != nil {
-			return err
-		}
-		binary.Write(w, binary.LittleEndian, uint32(ch))
-	}
+    for _, v := range items[1:] {
+        ch, err := strconv.Atoi(v)
+        if err != nil {
+            return err
+        }
+        binary.Write(w, binary.LittleEndian, uint32(ch))
+    }
 
-	return nil
+    return nil
 }
